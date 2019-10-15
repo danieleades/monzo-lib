71cb8f86e1efb50534e4f3758c602907afcd2242
Daniel Eades <danieleades@hotmail.com>
2019-10-15 12:07:29 +0100

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,14 @@
 #![warn(clippy::all)]
 
 //! [![Latest Docs](https://docs.rs/harbourmaster/badge.svg)](https://docs.rs/harbourmaster/)
-//! 
+//!
 //! This crate is an async Monzo API client in pure rust.
-//! 
+//!
 //! It is intended as the backend of a monzo CLI app that i'll probably
 //! never get to building.
-//! 
+//!
 //! In order to use this client, you will first need to get an access token and/or refresh token for the Monzo API (see [the docs](https://docs.monzo.com/))
-//! 
+//!
 //! ## Usage
 //! ```no_run
 //! use monzo_lib::{Client, Result};
@@ -20,13 +20,13 @@
 //!         .access_token("ACCESS_TOKEN")
 //!         .refresh_token("REFRESH_TOKEN")
 //!         .build();
-//! 
+//!
 //!     let accounts = client.accounts().await?;
-//! 
+//!
 //!     let account_id = &accounts[0].id;
-//! 
+//!
 //!     let balance = client.balance(account_id).await?;
-//! 
+//!
 //!     Ok(())
 //! }
 //! ```
