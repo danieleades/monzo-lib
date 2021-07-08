1f412da9f9627d04ff4ab08a01600d3288556a66
Bradley Garrod <32489229+BreD1810@users.noreply.github.com>
2021-07-08 11:46:06 +0100

    Change Form to Query for transaction list (#14)
    * Change Form to Query for transaction list
    
    * Add list_transactions example

---
+++ examples/list_transactions.rs
@@ -0,0 +1,33 @@
+use chrono::{Duration, Utc};
+use clap::Clap;
+use monzo::Client;
+
+#[derive(Clap)]
+struct Args {
+    access_token: String,
+    #[clap(long, default_value = "2")]
+    limit: u16,
+    #[clap(long, default_value = "89")]
+    days: i64,
+}
+
+#[tokio::main]
+async fn main() -> monzo::Result<()> {
+    let args = Args::parse();
+    let client = Client::new(args.access_token);
+
+    let accounts = client.accounts().await?;
+    let account_id = &accounts[0].id;
+
+    let transactions = client
+        .transactions(account_id)
+        .since(Utc::now() - Duration::days(args.days))
+        .limit(args.limit)
+        .send()
+        .await?;
+
+    println!("account: {}", account_id);
+    transactions.iter().for_each(|t| println!("\t{}", t.id));
+
+    Ok(())
+}
